4221cbab22eea8842eb91e0729fb490881db1e20
nlin17 <155456184+nlin17@users.noreply.github.com>
2025-01-28 16:23:16 +0000

    started anagrams

--- p1.py
+++ p1.py
@@ -1,2 +1,11 @@
 def group_anagrams(words):
-    pass
+    anagrams = {}
+    for word in words:
+        str = ''.join(sorted(word))
+        anagrams[str] = word
+
+
+# sort the letters
+
+words = ["bat", "meta", "tab", "tea", "ate", "tame", "jazz", "eat", "elephant", "phetenal"]
+group_anagrams(words)
