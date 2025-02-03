a6865c706bb3e52bc1af189e92bfc679a0218caf
nlin17 <155456184+nlin17@users.noreply.github.com>
2025-02-03 06:17:23 +0000

    wrote test frequency function

--- p1.py
+++ p1.py
@@ -1,11 +1,20 @@
 def group_anagrams(words):
     anagrams = {}
     for word in words:
-        str = ''.join(sorted(word))
-        anagrams[str] = word
+        ben = ''.join(sorted(word))
+        if ben not in anagrams:
+            anagrams[ben] = [word]
+        else:
+            anagrams[ben].append(word)
+        print(anagrams)
+    idk = []
+    for i in anagrams:
+        idk.append(anagrams[i])
+    return idk
 
 
 # sort the letters
 
+
 words = ["bat", "meta", "tab", "tea", "ate", "tame", "jazz", "eat", "elephant", "phetenal"]
-group_anagrams(words)
+print(group_anagrams(words))
