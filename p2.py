c196c36091087d220a04e9b423878db7d064c1e6
nlin17 <155456184+nlin17@users.noreply.github.com>
2025-02-04 18:01:11 +0000

    thank you ben for reminding me to push my code

--- p2.py
+++ p2.py
@@ -7,7 +7,7 @@ def k_most_frequent(lst, k):
             leaderboard[i] = 1
         else:
             leaderboard[i] += 1
-    leaderboard = dict(sorted(leaderboard.items(), key=lambda item: item[1], reverse=True))
+    leaderboard = dict(sorted(leaderboard.items(), key=lambda item: item[1], reverse=True)) # sorting the dictionary by decreasing value
     print(leaderboard)
     x = grud.unique(list(leaderboard.values())).size == 1 # test if all values are the same, so can return all keys rather than just :k keys
     print(x)
